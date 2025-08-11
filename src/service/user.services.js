36a85d98229a53334ab2e9fcc9913d3f4d8d2fdf
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-08-11 13:40:54 -0300

    Feat:Adicoinando criptografia na senha

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,9 +1,14 @@
 import userRepository from "../repositories/user.repositories.js";
+import bcrypt from 'bcrypt'
+
 
 async function createUserService(newUser){
     const foundUser = await userRepository.findUserByEmailRepository(newUser.email)
     if(foundUser) throw new Error("User already exists")
-    const user = await userRepository.createUserRepository(newUser)
+
+    const passHash = await bcrypt.hash(newUser.password, 10)
+    const user = await userRepository.createUserRepository({...newUser,password: passHash})
+    if(!user) throw new Error("Erro ao criar usuario")
     return user;
 }
 
