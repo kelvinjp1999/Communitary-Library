420cd1c49fff1af862eeef9c6ac1bcbdcb9c490a
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-08-11 14:33:44 -0300

    Feat:Sxhema

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import {Router} from 'express'
 import userController from '../controller/user.controllers.js'
+import {validate} from '../middlewares/validation.middlewares.js'
+import {userSchema} from '../schema/user.schema.js'
 
 const router = Router();
 
-router.post('/users', userController.creatUserController)
+router.post('/users', validate(userSchema) , userController.creatUserController)
 
 export default router
--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -2,9 +2,9 @@ import {z} from 'zod'
 
 const userSchema = z.object({
     username : z.string().min(3,'Username is required'),
-    email: z.string.email('Invalid email'),
-    password: z.string.min(6, 'Password must be 6 charactres'),
-    avatar: z.string.url('Invalid URl').optional()
+    email: z.string().email('Invalid email'),
+    password: z.string().min(6, 'Password must be 6 charactres'),
+    avatar: z.string().url('Invalid URl').optional()
 })
 
 export {userSchema}
